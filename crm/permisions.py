173d21f769e641cf62e078750e6c304732691ddf
adilkan <adilkanio555@gmail.com>
2022-09-29 02:20:55 +0600

    permisions

---
+++ crm/permisions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsClient(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and not request.user.is_staff)
+
+
+class IsCreditSpec(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and  request.user.occupation == 'Кредит.спец' or request.user.is_superuser)
+
+
+class IsCreditAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and  request.user.occupation == 'Кредит.админ' or request.user.is_superuser)
