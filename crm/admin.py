188e9443aefd00821710e2b213ca85600b5ed7dd
Sulaimanovuran <sulaimanovuran@gmail.com>
2022-09-30 22:49:17 +0600

    Изменен файл crm.models, добавлена новая логика в views.Client,Entity,DataKK

--- crm/admin.py
+++ crm/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
+
 from .models import *
 
 admin.site.register(Client)
 admin.site.register(Entity)
-admin.site.register(CreditSpecialist)
-admin.site.register(Occupation)
+
 admin.site.register(Company)
 admin.site.register(Guarantor)
 admin.site.register(Files)
@@ -21,13 +21,13 @@ class ImageInAdmin(admin.TabularInline):
     fields = ['image']
     max_num = 5
 
+
 class PropertyAdmin(admin.ModelAdmin):
     inlines = [FileInAdmin, ImageInAdmin]
 
 
 admin.site.register(Property, PropertyAdmin)
-admin.site.register(TelephoneConversation)
-admin.site.register(MeetConversation)
+admin.site.register(Conversation)
 admin.site.register(DataKK)
 admin.site.register(Activity)
 admin.site.register(Images)
