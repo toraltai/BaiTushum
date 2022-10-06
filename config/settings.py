854386557fad152fe724ec5fb20135ec5b5e7b96
toraltai <flangkg@gmail.com>
2022-10-06 23:59:10 +0600

    demo

--- config/settings.py
+++ config/settings.py
@@ -136,11 +136,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        # DATABASES = {
-        #     'default': {
-        #         'ENGINE': 'django.db.backends.sqlite3',
-        #         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
