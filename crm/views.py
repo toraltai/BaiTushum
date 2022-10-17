c45a6090dc811bbe017d143d5768828332f05c29
toraltai <flangkg@gmail.com>
2022-10-17 21:31:53 +0600

    bug

--- crm/views.py
+++ crm/views.py
@@ -61,9 +61,9 @@ class APIProperty(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
-        if self.request.user.specuser.occupation == 'Кредит.спец':
+        if self.request.user.spec_user.occupation == 'Кредит.спец':
             return SerializerProperty
-        elif self.request.user.specuser.occupation == 'Кредит.админ':
+        elif self.request.user.spec_user.occupation == 'Кредит.админ':
             return SerializerPropertyAdmin
         else:
             return redirect('/')
