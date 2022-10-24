427f400fbffe26f898a83b29da2f8c937ad28b0b
toraltai <flangkg@gmail.com>
2022-10-24 20:17:11 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  #if config('DEBUG') == '1' else False
+DEBUG = True if config('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
