3cc66b9ee7bcbc0811224a69e0c91c68bef9f010
toraltai <flangkg@gmail.com>
2023-01-11 21:17:23 +0600

    fix#1

--- crm/serializers.py
+++ crm/serializers.py
@@ -69,8 +69,8 @@ class ImagesSerializer(serializers.ModelSerializer):
 
 
 class SerializerPropertyAdmin(serializers.ModelSerializer):
-    type = serializers.ReadOnlyField()
-    address = serializers.ReadOnlyField()
+    # type = serializers.ReadOnlyField()
+    # address = serializers.ReadOnlyField()
     files = FilesSerializer(many=True, read_only=True, )
     images = ImagesSerializer(many=True, read_only=True)
 
