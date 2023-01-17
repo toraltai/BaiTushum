b2d09e2c0e3db95c24d6073c75dd180999c693bf
toraltai <flangkg@gmail.com>
2023-01-17 14:35:12 +0600

    company/activity

--- crm/serializers.py
+++ crm/serializers.py
@@ -34,6 +34,11 @@ class SerializerCompany(serializers.ModelSerializer):
         model = Company
         fields = '__all__'
 
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['field_activity'] = ActivitySerializer(instance.field_activity).data['activites_add']
+        return rep
+
 
 class FilesSerializer(serializers.ModelSerializer):
     class Meta:
