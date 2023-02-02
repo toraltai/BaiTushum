ffa1369d970895c6b59ab55d21569ca540984909
toraltai <flangkg@gmail.com>
2023-02-03 05:15:43 +0600

    fix

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger')),
+    path('users/', include('users.urls')),
+    path('crm/', include('crm.urls')),
     path('jet/', include('jet.urls')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-    path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger')),
-    path('crm/', include('crm.urls')),
     path('', admin.site.urls),
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
